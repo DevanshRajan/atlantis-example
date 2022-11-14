9fac0ed6eb40aab48df024778fd320bd175950d3
DevanshRajan <71208917+DevanshRajan@users.noreply.github.com>
2022-11-15 02:45:07 +0530

    added security group

--- main.tf
+++ main.tf
@@ -22,3 +22,14 @@ resource "aws_subnet" "first" {
     Name = "myfirstsubnet"
   }
 }
+
+resource "aws_security_group" "dev_example" {
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
